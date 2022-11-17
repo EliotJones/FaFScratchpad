483fc32a9107f30e52028b7ca4755a19c57bf50e
Eliot Jones <elioty@hotmail.co.uk>
2022-11-16 22:25:59 -0500

    start the project

---
+++ FaFPlayground.Tests/CompressedLabelTreeCompressTests.cs
@@ -0,0 +1,309 @@
+namespace FaFPlayground.Tests;
+
+public class CompressedLabelTreeCompressTests
+{
+    private const bool t = true;
+    private const bool f = false;
+
+    [Theory]
+    [InlineData(2)]
+    [InlineData(1)]
+    public void TwoByTwoIsFine(int compressionThreshold)
+    {
+        var treeRoot = GivenTreeWithCompressionLevel(2);
+
+        var cache = new NavLabelCache(
+            new List<IReadOnlyList<bool>>
+            {
+                new[] { t, t },
+                new[] { t, t }
+            });
+
+        treeRoot.Compress(cache, compressionThreshold);
+
+        Assert.True(treeRoot.Label);
+        Assert.Equal(1, Globals.NavLayerData[NavLayers.Air].PathableLeafs);
+        Assert.Equal(0, Globals.NavLayerData[NavLayers.Air].UnpathableLeafs);
+    }
+
+    [Fact]
+    public void TwoByTwoIsNotFineButNoFurtherCompression()
+    {
+        var treeRoot = GivenTreeWithCompressionLevel(2);
+
+        var cache = new NavLabelCache(
+            new List<IReadOnlyList<bool>>
+            {
+                new[] { t, t },
+                new[] { t, f }
+            });
+
+        treeRoot.Compress(cache, 2);
+
+        Assert.False(treeRoot.Label);
+        Assert.Equal(0, Globals.NavLayerData[NavLayers.Air].PathableLeafs);
+        Assert.Equal(1, Globals.NavLayerData[NavLayers.Air].UnpathableLeafs);
+    }
+
+    [Theory]
+    [InlineData(1, 1, 3)]
+    [InlineData(1, 0, 2)]
+    [InlineData(0, 1, 1)]
+    [InlineData(0, 0, 0)]
+    public void TwoByTwoIsNotFineFurtherCompression(int row, int col, int index)
+    {
+        var treeRoot = GivenTreeWithCompressionLevel(2);
+
+        var navData = new List<bool[]>
+        {
+            new[] { t, t },
+            new[] { t, t }
+        };
+
+        navData[row][col] = false;
+
+        var cache = new NavLabelCache(navData);
+
+        treeRoot.Compress(cache, 1);
+
+        Assert.Null(treeRoot.Label);
+        Assert.Equal(1, Globals.NavLayerData[NavLayers.Air].Subdivisions);
+        Assert.Equal(4, treeRoot.Children.Count);
+
+        for (int i = 0; i < treeRoot.Children.Count; i++)
+        {
+            var child = treeRoot.Children[i];
+
+            if (i == index)
+            {
+                Assert.False(child.Label);
+            }
+            else
+            {
+                Assert.True(child.Label);
+            }
+        }
+    }
+
+    [Theory]
+    [InlineData(1)]
+    [InlineData(2)]
+    [InlineData(4)]
+    public void FourByFourFine(int compressionLevel)
+    {
+        var treeRoot = GivenTreeWithCompressionLevel(4);
+
+        var cache = new NavLabelCache(
+            new List<IReadOnlyList<bool>>
+            {
+                new[] { t, t, t, t },
+                new[] { t, t, t, t },
+                new[] { t, t, t, t },
+                new[] { t, t, t, t }
+            });
+
+        treeRoot.Compress(cache, compressionLevel);
+
+        Assert.True(treeRoot.Label);
+        Assert.Equal(1, Globals.NavLayerData[NavLayers.Air].PathableLeafs);
+        Assert.Equal(0, Globals.NavLayerData[NavLayers.Air].UnpathableLeafs);
+    }
+
+    [Fact]
+    public void FourByFourSingleNotFineCompressionLevel2()
+    {
+        var treeRoot = GivenTreeWithCompressionLevel(4);
+
+        var cache = new NavLabelCache(
+            new List<IReadOnlyList<bool>>
+            {
+                new[] { t, t, t, t },
+                new[] { t, t, f, t },
+                new[] { t, t, t, t },
+                new[] { t, t, t, t }
+            });
+
+        treeRoot.Compress(cache, 2);
+
+        Assert.Null(treeRoot.Label);
+        Assert.Equal(1, Globals.NavLayerData[NavLayers.Air].Subdivisions);
+        Assert.Equal(3, Globals.NavLayerData[NavLayers.Air].PathableLeafs);
+        Assert.Equal(1, Globals.NavLayerData[NavLayers.Air].UnpathableLeafs);
+        Assert.Equal(4, treeRoot.Children.Count);
+
+        for (var i = 0; i < treeRoot.Children.Count; i++)
+        {
+            var child = treeRoot.Children[i];
+
+            if (i == 1)
+            {
+                Assert.False(child.Label);
+            }
+            else
+            {
+                Assert.True(child.Label);
+            }
+        }
+    }
+
+    [Fact]
+    public void FourByFourSingleNotFineCompressionLevel1()
+    {
+        var treeRoot = GivenTreeWithCompressionLevel(4);
+
+        var cache = new NavLabelCache(
+            new List<IReadOnlyList<bool>>
+            {
+                new[] { t, t, t, t },
+                new[] { t, t, f, t },
+                new[] { t, t, t, t },
+                new[] { t, t, t, t }
+            });
+
+        /*
+         * Divide into 7 blocks:
+         * 1:
+         * t t
+         * t t
+         *
+         * 2 - 5:
+         * t | t | f | t
+         *
+         * 6:
+         * t t
+         * t t
+         *
+         * 7:
+         * t t
+         * t t
+         */
+
+        treeRoot.Compress(cache, 1);
+
+        Assert.Null(treeRoot.Label);
+        Assert.Equal(2, Globals.NavLayerData[NavLayers.Air].Subdivisions);
+        Assert.Equal(6, Globals.NavLayerData[NavLayers.Air].PathableLeafs);
+        Assert.Equal(1, Globals.NavLayerData[NavLayers.Air].UnpathableLeafs);
+        Assert.Equal(4, treeRoot.Children.Count);
+
+        for (var i = 0; i < treeRoot.Children.Count; i++)
+        {
+            var child = treeRoot.Children[i];
+
+            if (i == 1)
+            {
+                Assert.Null(child.Label);
+                Assert.Equal(4, child.Children.Count);
+                for (var j = 0; j < child.Children.Count; j++)
+                {
+                    var grandchild = child.Children[j];
+
+                    if (j == 2)
+                    {
+                        Assert.False(grandchild.Label);
+                    }
+                    else
+                    {
+                        Assert.True(grandchild.Label);
+                    }
+                }
+            }
+            else
+            {
+                Assert.True(child.Label);
+            }
+        }
+    }
+
+    [Fact]
+    public void FourByFourTwoNotFineCompressionLevel1()
+    {
+        var treeRoot = GivenTreeWithCompressionLevel(4);
+
+        var cache = new NavLabelCache(
+            new List<IReadOnlyList<bool>>
+            {
+                new[] { t, t, t, t },
+                new[] { t, t, f, t },
+                new[] { f, t, t, t },
+                new[] { t, t, t, t }
+            });
+
+        /*
+         * Divide into 7 blocks:
+         * 1:
+         * t t
+         * t t
+         *
+         * 2 - 5:
+         * t | t | f | t
+         *
+         * 6 - 9
+         * f | t | t | t
+         *
+         * 10:
+         * t t
+         * t t
+         */
+
+        treeRoot.Compress(cache, 1);
+
+        Assert.Null(treeRoot.Label);
+        Assert.Equal(3, Globals.NavLayerData[NavLayers.Air].Subdivisions);
+        Assert.Equal(8, Globals.NavLayerData[NavLayers.Air].PathableLeafs);
+        Assert.Equal(2, Globals.NavLayerData[NavLayers.Air].UnpathableLeafs);
+        Assert.Equal(4, treeRoot.Children.Count);
+
+        for (var i = 0; i < treeRoot.Children.Count; i++)
+        {
+            var child = treeRoot.Children[i];
+
+            if (i == 1)
+            {
+                Assert.Null(child.Label);
+                Assert.Equal(4, child.Children.Count);
+                for (var j = 0; j < child.Children.Count; j++)
+                {
+                    var grandchild = child.Children[j];
+
+                    if (j == 2)
+                    {
+                        Assert.False(grandchild.Label);
+                    }
+                    else
+                    {
+                        Assert.True(grandchild.Label);
+                    }
+                }
+            }
+            else if (i == 2)
+            {
+                Assert.Null(child.Label);
+                Assert.Equal(4, child.Children.Count);
+                for (var j = 0; j < child.Children.Count; j++)
+                {
+                    var grandchild = child.Children[j];
+
+                    if (j == 0)
+                    {
+                        Assert.False(grandchild.Label);
+                    }
+                    else
+                    {
+                        Assert.True(grandchild.Label);
+                    }
+                }
+            }
+            else
+            {
+                Assert.True(child.Label);
+            }
+        }
+    }
+
+    private static CompressedLabelTree GivenTreeWithCompressionLevel(int level)
+    {
+        Globals.NavLayerData = new NavLayerData();
+        return new(NavLayers.Air, 0, 0, level, 0, 0);
+    }
+}
---
+++ FaFPlayground/NavData.cs
@@ -0,0 +1,181 @@
+﻿namespace FaFPlayground;
+
+// Reference code from: https://github.com/FAForever/fa/blob/deploy/fafdevelop/lua/sim/NavGenerator.lua#L281
+public static class Globals
+{
+    public static NavLayerData NavLayerData = new NavLayerData();
+}
+
+public enum NavLayers
+{
+    Air,
+    Land,
+    Amphibious
+}
+
+public class NavLayerDataItem
+{
+    public int Subdivisions;
+    public int PathableLeafs = 0;
+    public int UnpathableLeafs = 0;
+    public int Neighbors = 0;
+    public int Labels = 0;
+}
+
+public class NavLayerData
+{
+    private readonly Dictionary<NavLayers, NavLayerDataItem> _layers = new Dictionary<NavLayers, NavLayerDataItem>();
+
+    public NavLayerDataItem this[NavLayers layer]
+    {
+        get
+        {
+            if (!_layers.TryGetValue(layer, out var item))
+            {
+                item = new NavLayerDataItem();
+                _layers.Add(layer, item);
+            }
+
+            return item;
+        }
+    }
+}
+
+public class NavLabelCache
+{
+    private readonly IReadOnlyList<IReadOnlyList<bool>> _labels;
+
+    public NavLabelCache(
+        IReadOnlyList<IReadOnlyList<bool>> labels)
+    {
+        _labels = labels;
+    }
+
+    public IReadOnlyList<bool> this[int z] => _labels[z];
+}
+
+public class CompressedLabelTree
+{
+    private readonly NavLayers _layer;
+    private readonly int _c;
+    private readonly int _ox;
+    private readonly int _oz;
+    private readonly int _bx;
+    private readonly int _bz;
+
+    public readonly List<CompressedLabelTree> Children = new List<CompressedLabelTree>();
+
+    private bool? _label;
+    public bool? Label => _label;
+
+    public CompressedLabelTree(NavLayers layer, int bx, int bz, int c, int ox, int oz)
+    {
+        _layer = layer;
+        _c = c;
+        _ox = ox;
+        _oz = oz;
+        _bx = bx;
+        _bz = bz;
+    }
+
+    public void Compress(NavLabelCache rCache, int compressionThreshold)
+    {
+        var uniform = true;
+        var value = rCache[_oz][_ox];
+        if (_c <= compressionThreshold)
+        {
+            for (int z = _oz; z < _oz + _c; z++)
+            {
+                for (int x = _ox; x < _ox + _c; x++)
+                {
+                    uniform &= rCache[z][x];
+
+                    if (!uniform)
+                    {
+                        break;
+                    }
+                }
+
+                if (!uniform)
+                {
+                    break;
+                }
+            }
+
+            if (uniform)
+            {
+                _label = value;
+
+                if (_label == true)
+                {
+                    Globals.NavLayerData[_layer].PathableLeafs = Globals.NavLayerData[_layer].PathableLeafs + 1;
+                }
+                else
+                {
+                    Globals.NavLayerData[_layer].UnpathableLeafs = Globals.NavLayerData[_layer].UnpathableLeafs + 1;
+                }
+            }
+            else
+            {
+                _label = false;
+                Globals.NavLayerData[_layer].UnpathableLeafs = Globals.NavLayerData[_layer].UnpathableLeafs + 1;
+            }
+
+            return;
+        }
+
+        // recursive case where we do make children
+
+        value = rCache[_oz][_ox];
+        uniform = true;
+        for (int z = _oz; z < _oz + _c; z++)
+        {
+            for (int x = _ox; x < _ox + _c; x++)
+            {
+                uniform &= rCache[z][x];
+
+                if (!uniform)
+                {
+                    break;
+                }
+            }
+
+            if (!uniform)
+            {
+                break;
+            }
+        }
+
+        if (uniform)
+        {
+            // we're uniform, so we're good
+            _label = value;
+
+            if (_label == true)
+            {
+                Globals.NavLayerData[_layer].PathableLeafs = Globals.NavLayerData[_layer].PathableLeafs + 1;
+            }
+            else
+            {
+                Globals.NavLayerData[_layer].UnpathableLeafs = Globals.NavLayerData[_layer].UnpathableLeafs + 1;
+            }
+        }
+        else
+        {
+            // We're not uniform, split up to children
+            var hc = (int)(0.5 * _c);
+
+            Children.Add(new CompressedLabelTree(_layer, _bx, _bz, hc, _ox, _oz));
+            Children.Add(new CompressedLabelTree(_layer, _bx, _bz, hc, _ox + hc, _oz));
+            Children.Add(new CompressedLabelTree(_layer, _bx, _bz, hc, _ox, _oz + hc));
+            Children.Add(new CompressedLabelTree(_layer, _bx, _bz, hc, _ox + hc, _oz + hc));
+
+            foreach (var child in Children)
+            {
+                child.Compress(rCache, compressionThreshold);
+            }
+
+            Globals.NavLayerData[_layer].Subdivisions = Globals.NavLayerData[_layer].Subdivisions + 1;
+        }
+    }
+}
